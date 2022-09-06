aef66041674715535f96e45d9bd22d1203d6152b
aadowst <105029233+aadowst@users.noreply.github.com>
2022-09-06 15:09:30 -0700

    initial commit

---
+++ MERN_Coding_Dojo/React/APIs/Coin_Flipping.js
@@ -0,0 +1,48 @@
+//Goal:  Rewrite the synchronous function below into on that is asynch.
+function tossCoin(){
+    return Math.random() > 0.5 ? "heads" : "tails";
+}
+
+function fiveHeadsSync() {
+    let headsCount = 0;
+    let attempts = 0;
+    while(headsCount < 5) {
+        attempts++;
+        let result = tossCoin();
+        console.log(`${result} was flipped`);
+        if(result === "heads") {
+            headsCount++;
+        } else {
+            headsCount = 0;
+        }
+    }
+    return `It took ${attempts} tries to flip five "heads"`;
+}
+// console.log( fiveHeadsSync() );
+// console.log( "This is run after the fiveHeadsSync function completes" );
+
+function fiveHeadsAsync(){
+    return new Promise( (resolve, reject) => {
+        let headsCount = 0;
+        let attempts = 0;
+        while(headsCount < 5) {
+            attempts++;
+            let result = tossCoin();
+            if(result === "heads") {
+                headsCount++;
+            } else {
+                headsCount = 0;
+            }
+        }
+        if(headsCount >=5){
+            resolve(`It took ${attempts} tries to flip five "heads"`)
+        }else{
+            reject("no good")
+        }
+    });
+}
+fiveHeadsAsync()
+.then( res => console.log(res))
+.catch(err => console.log(err))
+
+console.log("end of code");
