8fbf465b4f8ba4176ca9c8535ce4bef2ed90cf01
aadowst <105029233+aadowst@users.noreply.github.com>
2022-09-08 11:02:59 -0700

    distinguished isLoading = false & hasData = true

--- MERN_Coding_Dojo/React/React_Routing/luke-apiwalker/src/components/Display.js
+++ MERN_Coding_Dojo/React/React_Routing/luke-apiwalker/src/components/Display.js
@@ -1,61 +1,66 @@
-import { useParams, useNavigate} from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from 'react'
 
 const Display = () => {
     const navigate = useNavigate();
     const { resource, id } = useParams();
-    const [resourceSearched, setResourceSearched] = useState()
-    const [isLoading, setIsLoading] = useState(true)
+    const [resourceSearched, setResourceSearched] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
 
 
     useEffect(() => {
+        setIsLoading(true);
         axios.get(`https://swapi.dev/api/${resource}/${id}`)
             .then(res => {
                 console.log(res.data)
                 setResourceSearched(res.data);
-                setIsLoading(false)
+
             }).catch(err => {
                 console.log(err);
                 navigate('/error');
-            })
-
-    }, [resource, id])
-
-const handleClick = (e, url) => {
-    const splitUrl = url.split("/");
-    // extract the id number from the homeworld url and call the api again
-    navigate(`/planets/${splitUrl[5]}`)
-    
-}
-
-if(!isLoading){
-    switch (resource) {
-        case "people":
-            return (
-                <div>
-                    <h1>{resourceSearched.name}</h1>
-                    <h2>Birth Year: <span style={{ fontWeight: "lighter" }}>{resourceSearched.birth_year}</span></h2>
-                    <h2>Height: <span style={{ fontWeight: "lighter" }}>{resourceSearched.height} cm</span></h2>
-                    <h2>Mass: <span style={{ fontWeight: "lighter" }}>{resourceSearched.mass} kg</span></h2>
-                    <h2>Homeworld: <span style={{ fontWeight: "lighter" }} onClick={(e) => handleClick(e, resourceSearched.homeworld)}>Click Here</span></h2>
-
-                </div>
-            )
-        case "planets":
-            return (
-                <div>
-                    <h1>{resourceSearched.name}</h1>
-                    <h2>Gravity: <span style={{ fontWeight: "lighter" }}>{resourceSearched.gravity}</span></h2>
-                    <h2>Climate: <span style={{ fontWeight: "lighter" }}>{resourceSearched.climate}</span></h2>
-                    <h2>Population: <span style={{ fontWeight: "lighter" }}>{resourceSearched.population}</span></h2>
-                </div>
+            }).finally(() =>
+                {console.log("finally")
+                    setIsLoading(false)}
             )
 
-        default:
-            return (<p>you searched for a  {resource} with an id of {id}</p>)
+    }, [resource, id, navigate])
+
+    const handleClick = (e, url) => {
+        const splitUrl = url.split("/");
+        // extract the id number from the homeworld url and call the api again
+        navigate(`/planets/${splitUrl[5]}`)
+
     }
-    }else{
+
+    if (!isLoading) {
+        if(resourceSearched === null) return "no data";
+        switch (resource) {
+            case "people":
+                return (
+                    <div>
+                        <h1>{resourceSearched.name}</h1>
+                        <h2>Birth Year: <span style={{ fontWeight: "lighter" }}>{resourceSearched.birth_year}</span></h2>
+                        <h2>Height: <span style={{ fontWeight: "lighter" }}>{resourceSearched.height} cm</span></h2>
+                        <h2>Mass: <span style={{ fontWeight: "lighter" }}>{resourceSearched.mass} kg</span></h2>
+                        <h2>Homeworld: <span style={{ fontWeight: "lighter" }} onClick={(e) => handleClick(e, resourceSearched.homeworld)}>Click Here</span></h2>
+
+                    </div>
+                )
+            case "planets":
+                return (
+                    <div>
+                        <h1>{resourceSearched.name}</h1>
+                        <h2>Gravity: <span style={{ fontWeight: "lighter" }}>{resourceSearched.gravity}</span></h2>
+                        <h2>Climate: <span style={{ fontWeight: "lighter" }}>{resourceSearched.climate}</span></h2>
+                        <h2>Population: <span style={{ fontWeight: "lighter" }}>{resourceSearched.population}</span></h2>
+                    </div>
+                )
+
+            default:
+                return (<p>you searched for a  {resource} with an id of {id}</p>)
+        }
+    } else {
         return "Loading"
     }
 }
