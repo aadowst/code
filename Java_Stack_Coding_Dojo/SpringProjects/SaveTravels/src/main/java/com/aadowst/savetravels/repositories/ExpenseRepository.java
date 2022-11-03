918225dbb28a8e5f64697c13ed4335f1a9da9be2
Adrian Dowst <105029233+aadowst@users.noreply.github.com>
2022-11-03 12:57:13 -0700

    Delete Java_Stack_Coding_Dojo directory

--- Java_Stack_Coding_Dojo/SpringProjects/SaveTravels/src/main/java/com/aadowst/savetravels/repositories/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aadowst.savetravels.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.aadowst.savetravels.models.Expense;
-
-public interface ExpenseRepository extends CrudRepository<Expense, Long>{
-
-}
