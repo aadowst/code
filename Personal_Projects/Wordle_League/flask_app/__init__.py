169b397a0b06603b4b84a95fede70bf330f0f43e
Adrian Dowst <105029233+aadowst@users.noreply.github.com>
2022-11-03 12:58:07 -0700

    Delete Personal_Projects directory

--- Personal_Projects/Wordle_League/flask_app/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-from flask import Flask, session
-app = Flask(__name__)
-app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
-
-from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt(app)
