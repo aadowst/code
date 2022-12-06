2a5c09a4bb413439aaf354ea29a0ba604082a1bf
aadowst <105029233+aadowst@users.noreply.github.com>
2022-12-06 10:30:47 -0800

    initial commit

---
+++ Learning_Typescript/Code_With_Mosh/Types/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let sales = 123456;
+let course = "TypeScript";
+let is_published = true;
+let mySize = "MEDIUM";
+console.log(mySize);
+//# sourceMappingURL=index.js.map
---
+++ Learning_Typescript/Code_With_Mosh/Types/src/index.ts
@@ -0,0 +1,7 @@
+let sales: number = 123456;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+
+const enum Size {Small = "SMALL", Medium="MEDIUM", Large="LARGE"}
+let mySize: Size = Size.Medium
+console.log(mySize)
