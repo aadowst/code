8f34164421ecb7b6f7c92ecc3d690e46118352ef
aadowst <105029233+aadowst@users.noreply.github.com>
2022-12-12 15:11:20 -0800

    examples of extending generic classes

--- Learning_Typescript/Code_With_Mosh/Generics/dist/index.js
+++ Learning_Typescript/Code_With_Mosh/Generics/dist/index.js
@@ -1,3 +1,25 @@
 "use strict";
 console.log("hello world");
+class Store {
+    constructor() {
+        this._objects = [];
+    }
+    add(object) {
+        this._objects.push(object);
+    }
+}
+class CompressibleStore extends Store {
+    compress() {
+    }
+}
+class SearchableStore extends Store {
+    search(name) {
+        return this._objects.find(obj => obj.name === name);
+    }
+}
+class ProductStore extends Store {
+    filterByCategory(category) {
+        return this._objects.filter(item => item.category === category);
+    }
+}
 //# sourceMappingURL=index.js.map
--- Learning_Typescript/Code_With_Mosh/Generics/src/index.ts
+++ Learning_Typescript/Code_With_Mosh/Generics/src/index.ts
@@ -1,1 +1,35 @@
 console.log("hello world");
+
+interface Product {
+  name: string;
+  price: number;
+	category: string;
+}
+
+class Store<T> {
+  protected _objects: T[] = [];
+
+  add(object: T): void {
+    this._objects.push(object);
+  }
+}
+
+class CompressibleStore<T> extends Store<T> {
+  compress() {
+    //insert code
+  }
+}
+
+//the generic type is constrained with an object that has a name property, so the find method can compare each entry's name with the name passed to the search method
+class SearchableStore<T extends { name: string }> extends Store<T>{
+	search(name: string): T | undefined {
+		return this._objects.find(obj => obj.name === name)
+	}
+}
+
+//fix the generic type (i.e. set it to specific type)
+class ProductStore extends Store<Product>{
+	filterByCategory(category: string): Product[]{
+		return this._objects.filter(item => item.category === category)
+	}
+}
