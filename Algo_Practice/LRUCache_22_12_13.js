22f308e6205add9e8455b5191c79ccedd18b60a0
aadowst <105029233+aadowst@users.noreply.github.com>
2022-12-13 15:42:06 -0800

    added cleaner solution

--- Algo_Practice/LRUCache_22_12_13.js
+++ Algo_Practice/LRUCache_22_12_13.js
@@ -1,5 +1,36 @@
 // https://leetcode.com/problems/lru-cache/
 
+// Comment:  Map objects are like arrays, becuase key-value pairs are added at the end. In this solution, when key-value pairs are to be modified, the original key is deleted and an identical key is created (with the value), putting it at the end. Therefore, the first element can be removed from the Map, if it is too big
+
+class LRUCacheMap{
+	constructor(capacity){
+		this.cache = new Map()
+		this.capacity = capacity
+	}
+
+	get(key){
+		if(!this.cache.has(key)) return -1
+		const value = this.cache.get(key)
+		this.cache.delete(key)
+		this.cache.set(key, value)
+		return value
+		}
+
+	put(key, value){
+		if(this.cache.has(key)){
+			this.cache.delete(key)
+		}
+		this.cache.set(key, value)
+		if(this.cache.size > this.cache.capacity){
+			//Least recently used element is at the beginning, so it will be the first element returned when .next() is called on the .keys() iterator
+			leastRecentKey = this.cache.keys().next().value
+			this.cache.delete(leastRecentKey)
+		}
+	}
+}
+
+
+// comment: added a rank property to each value, so the lowest rank (least used would be deleted)
 /**
  * @param {number} capacity
  */
