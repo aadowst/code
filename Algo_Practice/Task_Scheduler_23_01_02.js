bbdf38cdd8a233bd10d5d8ff9e905f91e9ccfd18
aadowst <105029233+aadowst@users.noreply.github.com>
2023-01-02 15:15:32 -0800

    much improved algebraic solution

--- Algo_Practice/Task_Scheduler_23_01_02.js
+++ Algo_Practice/Task_Scheduler_23_01_02.js
@@ -1,5 +1,36 @@
 // leetcode.com/problems/task-scheduler
 
+// Runtime:  97 ms (beats 92.95%)
+// Memory:  45.9 MB (beats 78.18%)
+// Comment: much better than solution below!
+
+var leastIntervalAlgebraic = function (tasks, n){
+	if (n === 0) return tasks.length;
+
+	// record frequencies of each letter in array
+	const charFreq = Array(26).fill(0)
+	for(const task of tasks){
+		const idx = task.charCodeAt(0) - "A".charCodeAt(0)
+		charFreq[idx]++
+	}
+
+	charFreq.sort((a,b) => b - a)  // sort in descending order
+
+	let largestFrequencyValue = charFreq[0]
+	let numCharsWithLargestFrequencyValue = 1
+
+	// determine if other characters have the same frequency as the first element of array
+	for(let i = 1; i < charFreq.length - 1; i++){
+		if(charFreq[i] === largestFrequencyValue) numCharsWithLargestFrequencyValue++
+		else break
+	}
+	const lengthOfRound = n + 1 
+	const roundsRequiringIdles = largestFrequencyValue - 1  // if there were no other characters, all but the last round would require idles
+	const timeRequiredByCharsWithLargestFreq = lengthOfRound * roundsRequiringIdles + numCharsWithLargestFrequencyValue  // add in the numChars... to account for the last round
+	return Math.max(timeRequiredByCharsWithLargestFreq, tasks.length)  // if the tasks.length is <= timeRequired... then the other tasks could replace most/all of the idles. If not, then the 'extra' tasks would need to be put in somewhere.
+} 
+
+
 // Runtime:  281 ms (beats 15.68%)
 // Memory Usage:  52.6 MB (beats 10.45%)
 // Comment:  definitely overthought it (other solutions solved algebraically).
@@ -9,7 +40,7 @@
  * @param {number} n
  * @return {number}
  */
-var leastInterval = function (tasks, n) {
+var leastIntervalPriorityQueue = function (tasks, n) {
   if (n === 0) return tasks.length;
 
   // loop through tasks, storing the freq of each in a hashmap/array
