908aaa2c8962cb6796e3736e0d94c5fc6d23a623
aadowst <105029233+aadowst@users.noreply.github.com>
2023-01-16 18:32:29 -0800

    another solution

--- Algo_Practice/Walls_and_Gates_23_01_16.js
+++ Algo_Practice/Walls_and_Gates_23_01_16.js
@@ -41,6 +41,51 @@ function distanceToGate(grid){
 	const maxRow = grid.length - 1
 	const maxColumn = grid[0].length - 1
 	const queue = []
+	let round = 0
+
+	function bfs(){
+		while(queue.length){
+			for(let i = 0; i < queue.length; i++){
+				const [row, column] = queue.shift()
+				grid[row][column] = round
+				checkCell(row - 1, column)
+				checkCell(row + 1, column)
+				checkCell(row, column - 1)
+				checkCell(row, column + 1)
+			}
+			round++
+		}
+	}
+	
+	function checkCell(row, column){
+		if(row < 0 || row > maxRow || column < 0 || column > maxColumn) return
+		if(visited.has(`${row},${column}`)) return
+		if(grid[row][column] == -1) return
+		visited.add(`${row},${column}`)
+		queue.push([row, column])
+	}
+
+	for(let row = 0; row <= maxRow; row++){
+		for(let column = 0; column <= maxColumn; column++){
+			if(grid[row][column] == 0) {
+				visited.add(`${row},${column}`)
+				queue.push([row, column])
+			}
+		}
+	}
+
+	bfs()
+	return grid
+}
+
+console.log(distanceToGate(grid1))
+
+function distanceToGateOriginal(grid){
+
+	let visited = new Set()
+	const maxRow = grid.length - 1
+	const maxColumn = grid[0].length - 1
+	const queue = []
 
 	function bfs(){
 		while(queue.length){
@@ -74,4 +119,4 @@ function distanceToGate(grid){
 	return grid
 }
 
-console.log(distanceToGate(grid1))
+// console.log(distanceToGateOriginal(grid1))
