54e59b73a13f785866c5d82f3669b3e37421424b
aadowst <105029233+aadowst@users.noreply.github.com>
2023-01-21 12:26:45 -0800

    refactored to use node rank and path compression

--- Algo_Practice/Number_Connected_Components_in_Undirected_Graph_23_01_21.js
+++ Algo_Practice/Number_Connected_Components_in_Undirected_Graph_23_01_21.js
@@ -11,7 +11,49 @@ const edges2 = [
   [6, 7],
 ]; // expected 3
 
-function numConnectedComponents(edges) {
+function numConnectedComponents(n, edges){
+	const rank = Array(n+1).fill(1)  // rank is the measure of how many children each node has (including itself)
+	const parent = Array(n+1)
+	let numComponents = n
+	for(let i = 0; i < parent.length; i++){
+		parent[i] = i
+	}
+
+	for (const [node1, node2] of edges) {
+    numComponents -= performUnion(node1, node2);
+  }
+
+	function performUnion(node1, node2){
+		const node1Parent = findParent(node1)
+		const node2Parent = findParent(node2)
+		if(node1Parent === node2Parent) return 0  // no union performed, so number of connected components doesn't change
+
+		if(rank[node2Parent] > rank[node1Parent]){  // component with more children adopts the smaller component
+			parent[node1Parent] = node2Parent
+			rank[node2Parent] += rank[node1Parent]
+		}
+		else{
+			parent[node2Parent] = node1Parent
+			rank[node1Parent] += rank[node2Parent]
+		}
+		return 1
+	}
+
+	function findParent(inputNode){
+		let node = inputNode
+		while(parent[node] !== node){
+			const nodeParent = parent[node]
+			parent[node] = parent[nodeParent] // path compression; set the parent of a node equal to its grandparent
+			node = parent[node]
+		}
+		return node
+	}
+	
+	return numComponents
+	}
+
+
+function numConnectedComponentsOriginal(edges) {
   const parent = Array(edges.length * 2 + 1).fill(-1);
   let maxVal = 0;
 
@@ -47,4 +89,4 @@ function numConnectedComponents(edges) {
 	return componentParents.size
 }
 
-console.log(numConnectedComponents(edges2))
+console.log(numConnectedComponents(8,edges2))
