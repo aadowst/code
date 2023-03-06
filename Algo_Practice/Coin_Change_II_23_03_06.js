4e2839ff2a47f66b4d8c98ddcd78b64112df86fd
aadowst <105029233+aadowst@users.noreply.github.com>
2023-03-06 09:42:46 -0800

    added 2D array solution

--- Algo_Practice/Coin_Change_II_23_03_06.js
+++ Algo_Practice/Coin_Change_II_23_03_06.js
@@ -1,9 +1,36 @@
 // https://leetcode.com/problems/coin-change-ii/
 
+// Runtime:  117 ms (beats 40.27%)
+// Memory:  65.5 MB (beats 29.33%)
+// Strategy:  two dimensional dp array (rows correspond to index of coin and columns correspond to values from zero to target amount)
+
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function(amount, coins) {
+	const dpArray = Array(coins.length + 1).fill(0).map(()=> Array(amount + 1).fill(0))
+
+	for(let currentAmount = 0; currentAmount <= amount; currentAmount++){
+			for(let coinIndex = coins.length - 1; coinIndex >= 0; coinIndex--){
+					if(currentAmount === 0) {
+							dpArray[coinIndex][currentAmount] = 1
+							continue
+					}
+					dpArray[coinIndex][currentAmount] += dpArray[coinIndex + 1][currentAmount]
+					const coinValue = coins[coinIndex]
+					if(currentAmount - coinValue < 0) continue
+					dpArray[coinIndex][currentAmount] += dpArray[coinIndex][currentAmount - coinValue]
+			}
+	}
+	return dpArray[0][amount]
+};
+
 // Runtime:  1153 ms (beats 14.13%)
 // Memory:  64.5 MB (beats 41.87%)
 // Strategy:  Memoized depth-first-search, avoiding duplicates by only recursively calling the function with coins of equal or greater value that the current coin
-var change = function(amount, coins) {
+var changeMemoizedDFS = function(amount, coins) {
 	const seen = new Map()
 	return dfs(0,0)
 
