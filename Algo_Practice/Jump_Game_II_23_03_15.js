e5e7530fefadfff69a2fe258039f9ad6e222311b
aadowst <105029233+aadowst@users.noreply.github.com>
2023-03-15 13:02:32 -0700

    refactored

--- Algo_Practice/Jump_Game_II_23_03_15.js
+++ Algo_Practice/Jump_Game_II_23_03_15.js
@@ -1,5 +1,24 @@
 // https://leetcode.com/problems/jump-game-ii/
 
+// Runtime:  63 ms (beats 88.86%)
+// Memory:  43.6 MB (beats 82.95%)
+// Comment:  refactored to improve readability. Same basic strategy as following solution. Slightly slower performance, because of for loop structure
+
+var jump = function(nums) {
+	const endIndex = nums.length - 1
+	let jumpCount = 0
+	let jumpStart = 0
+	let jumpEnd = 0
+	for(let i = 0; i < endIndex; i++){
+			jumpEnd = Math.max(jumpEnd, nums[i] + i)
+			if(i >= jumpStart){
+					jumpCount++
+					jumpStart = jumpEnd
+			}
+	}
+	return jumpCount
+};
+
 // Runtime:  56 ms (beats 98.16%)
 // Memory:  44.2 ms (beats 34.80%)
 // Strategy:  greedy
